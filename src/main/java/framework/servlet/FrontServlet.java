833d7ae1996dcd1aad4165a9515c4783b2f633ee
Andriatsara Iratra Fernand <Iratra53@gmail.com>
2025-11-07 22:19:38 +0300

    ajustement pour suivre la nouvelle logique

--- src/main/java/framework/servlet/FrontServlet.java
+++ src/main/java/framework/servlet/FrontServlet.java
@@ -2,18 +2,67 @@ package framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.http.HttpClient;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
+
+import framework.annotation.ControllerAnnot;
+import framework.annotation.UrlAnnot;
+
+import framework.util.ProjectConfig;
+import framework.util.ProjectScanner;
 
 @WebServlet("/")
 public class FrontServlet extends HttpServlet
 {
+    private Map<String, Class<?>> routes = new HashMap<>();
+
+    @Override
+    public void init() throws ServletException
+    {
+        super.init();
+        System.out.println("Test Init FrontServlet");
+
+        ProjectConfig config = new ProjectConfig();
+
+        String basePackage = config.getProperty("PACKAGE_RACINE");
+
+        System.out.println("Base package: " + basePackage);
+        
+        ProjectScanner scanner = new ProjectScanner(basePackage);
+        Set<Class<?>> projectClasses = scanner.getAllProjectClasses();
+        List<Class<?>> listClasses = new ArrayList<>(projectClasses);
+
+        for (Class<?> clazz : listClasses)
+        {
+            if (clazz.isAnnotationPresent(ControllerAnnot.class))
+            {
+                // Récupère les fonctions publiques
+                Method[] methods = clazz.getMethods();
+                for (Method method : methods)
+                {
+                    if (method.isAnnotationPresent(UrlAnnot.class))
+                    {
+                        UrlAnnot urlAnnot = method.getAnnotation(UrlAnnot.class);
+                        String url = urlAnnot.value();
+                        routes.put(url, clazz);
+                    }
+                }
+            }
+        }
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws ServletException, IOException
     {
@@ -26,17 +75,21 @@ public class FrontServlet extends HttpServlet
 
         boolean pathExists = getServletContext().getResource(path) != null;
 
-        if(pathExists && !path.equals("/") && !path.isEmpty()) 
+        PrintWriter writer = rep.getWriter();
+
+        if (routes.containsKey(path))
         {
-            RequestDispatcher dispatcher = getServletContext().getNamedDispatcher("default");
-            dispatcher.forward(req, rep);
-        } 
+            Class<?> clazz = routes.get(path);
+            String packageName = clazz.getPackageName();
+            writer.println(packageName + "." + clazz.getSimpleName() + " : " + path);
+        }
         // Introuvable
-        else 
+        else
         {
-            PrintWriter writer = rep.getWriter();
             writer.println("Chemin introuvable : ");
             writer.println(path);
+            rep.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+        
     }
 }
